bfc550984cab5ba6df08666d588c31ce40ab6bae
Lacro59 <lacro59@gmail.com>
2020-10-01 14:26:44 +0200

    Added PlayniteSDK 5.3.0-rc

--- CheckLocalizations.cs
+++ CheckLocalizations.cs
@@ -50,7 +50,7 @@ namespace CheckLocalizations
             string pluginFolder = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
 
             // Add plugin localization in application ressource.
-            PluginCommon.Localization.SetPluginLanguage(pluginFolder, api.Paths.ConfigurationPath);
+            PluginCommon.Localization.SetPluginLanguage(pluginFolder, api.ApplicationSettings.Language);
             // Add common in application ressource.
             PluginCommon.Common.Load(pluginFolder);
 
