3285fb4bb3e0705faa4a69d4009fdd232e53acf8
Lacro59 <lacro59@gmail.com>
2022-03-04 10:55:42 +0100

    updated LocalizationsDatabase()

--- source/Services/LocalizationsDatabase.cs
+++ source/Services/LocalizationsDatabase.cs
@@ -321,14 +321,7 @@ namespace CheckLocalizations.Services
                 }
                 catch (Exception ex)
                 {
-                    Common.LogError(ex, true);
-                    logger.Error($"Tag insert error with {game.Name}");
-
-                    PlayniteApi.Notifications.Add(new NotificationMessage(
-                        $"{PluginName}-Tag-Errors",
-                        $"{PluginName}\r\n" + resources.GetString("LOCCommonNotificationTagError"),
-                        NotificationType.Error
-                    ));
+                    Common.LogError(ex, false, $"Tag insert error with {game.Name}", true, PluginName, string.Format(resources.GetString("LOCCommonNotificationTagError"), game.Name));
                 }
             }
         }
@@ -351,14 +344,6 @@ namespace CheckLocalizations.Services
             PluginSettings.Settings.HasData = gameLocalizations.HasData;
             PluginSettings.Settings.HasNativeSupport = gameLocalizations.HasNativeSupport();
             PluginSettings.Settings.ListNativeSupport = gameLocalizations.Items;
-        }
-
-        public override void Games_ItemUpdated(object sender, ItemUpdatedEventArgs<Game> e)
-        {
-            foreach (var GameUpdated in e.UpdatedItems)
-            {
-                Database.SetGameInfo<Models.Localization>(PlayniteApi, GameUpdated.NewData.Id);
-            }
         }
     }
 }
